3eba28a85b0ee706e15ef0aabed57ec548bbfe2c
极风工作室 <3565583431@qq.com>
2026-07-23 09:13:32 +0800

    add: build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+// Top-level build file
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.5.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
+}

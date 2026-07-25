68e8a753886030a6578724967c01662bc98e1f66
极风工作室 <3565583431@qq.com>
2026-07-25 16:06:43 +0800

    fix(build): 恢复官方源，使用预装Android SDK

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,6 @@
 // Top-level build file
 buildscript {
     repositories {
-        maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { url = uri("https://jitpack.io") }
         google()
         mavenCentral()
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,13 +9,9 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
-        maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven { url = uri("https://maven.aliyun.com/repository/jcenter") }
-        maven { url = uri("https://jitpack.io") }
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
